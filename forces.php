f045a3bab6235dc480e2b942c5400f78d2052882
James Russo <1jlrusso@gmail.com>
2018-04-04 15:41:54 -0700

    added '#go-up-container' element to scroll page back to top

--- forces.php
+++ forces.php
@@ -416,6 +416,11 @@
 				</div>
 				<!-- End of Calculator Modal -->
 
+				<!-- Go back to top -->
+				<div id="go-up-container">
+					<button type="button" id="go-up-btn"><span class="glyphicon glyphicon-chevron-up"></span></button>
+				</div>
+
 	</section>
 
 	<script type="text/javascript" src="forces.js"></script>
